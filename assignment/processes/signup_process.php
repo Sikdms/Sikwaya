b73617c0b5a475ea072f1ac17e5a4a4c2a6a3ec1
sikwaya lwanzo <129254070+Sikdms@users.noreply.github.com>
2024-04-05 17:18:03 +0300

    signup_process.php

--- assignment/processes/signup_process.php
+++ assignment/processes/signup_process.php
@@ -1,40 +1,50 @@
 <?php
-    require_once "../config/dbConnect.php";
 
-    $firstname = mysqli_real_escape_string($dbConn, ucwords(strtolower($_POST["firstname"])));
-    $lastname = mysqli_real_escape_string($dbConn, ucwords(strtolower($_POST["lastname"])));
-    $email = mysqli_real_escape_string($dbConn, strtolower($_POST["email"]));
+ session_start();
+
+require_once "../config/dbConnect.php"; 
+
+//sign up process
+
+if (isset($_POST["signup"])){
+    $firstname = mysqli_real_escape_string($dbConn, $_POST["firstname"]);
+    $lastname = mysqli_real_escape_string($dbConn, $_POST["lastname"]);
+    $othername = mysqli_real_escape_string($dbConn, $_POST["othername"]);
     $gender = mysqli_real_escape_string($dbConn, $_POST["gender"]);
+    $dob = mysqli_real_escape_string($dbConn, $_POST["dob"]);
+    $username = mysqli_real_escape_string($dbConn, $_POST["username"]);
+    $email = mysqli_real_escape_string($dbConn, $_POST["email"]);
     $password = mysqli_real_escape_string($dbConn, $_POST["password"]);
+    $passwordconf = mysqli_real_escape_string($dbConn, $_POST["passwordconf"]);
     $userType = mysqli_real_escape_string($dbConn, $_POST["userType"]);
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        die('Invalid email');
+    
+    if($password != $passwordconf){
+        header("Location: ../signup.php");
+        exit();
     }
-
-    $email_exist = "SELECT email FROM users WHERE email='$email' LIMIT 1";
-    $email_exist_res = $dbConn->query($email_exist);
-    if($email_exist_res->num_rows > 0){
-        die('Email aleady exist'); 
+    $hash_password = password_hash($passwordconf, PASSWORD_DEFAULT);
+    
+    $user_insert= "INSERT INTO tbl_users(firstname,lastname,othername,gender,dob,username,email,password, userType, created)VALUES('$firstname','$lastname','$othername','$gender','$dob','$username','$email','$hash_password','$userType', UNIX_TIMESTAMP())";
+    
+    
+    if($dbConn->query($user_insert) === TRUE){
+        header("Location: ../Login.php");
+        exit();
     }
-
-    $hash_password = PASSWORD_HASH($pass, PASSWORD_DEFAULT);
-    $insert_qry = "INSERT INTO users(firstname,lastname, email, gender, password)VALUES('$firstname','$lastname', '$email', '$gender', '$hash_password','$userType', UNIX_TIMESTAMP())";
-
-    if($dbConn->query($insert_qry) === TRUE){
-        header("Location: ../login.php");
-    }else{
-        print "Process Failed" . $insert_qry . "<br>" . $dbConn->error;
+    else{
+        die("Failed to insert the record: " .$dbConn->error);
     }
+    
+}
 
-
-
- if(isset($_POST["login"])){
-    $entered_email = mysqli_real_escape_string($dbConn, $_POST["email"]);
+//login process
+ if(isset($_POST["Login"])){
+    $entered_username = mysqli_real_escape_string($dbConn, $_POST["username"]);
     $entered_password = mysqli_real_escape_string($dbConn, $_POST["password"]);
     
-    $spot_email = "SELECT*FROM tbl_users WHERE email = '$entered_email' LIMIT 1";
+    $spot_username = "SELECT*FROM tbl_users WHERE username = '$entered_username' LIMIT 1";
     
-    $uName_res = $dbConn->query($spot_email);
+    $uName_res = $dbConn->query($spot_username);
     
     if($uName_res->num_rows > 0){
         $_SESSION["control"] = $uName_res->fetch_assoc();
@@ -42,35 +52,37 @@
         $stored_password = $_SESSION["control"]["password"];
         $userType = $_SESSION["control"]["userType"];
         
-        
         if(password_verify($entered_password, $stored_password)){
     // die("$userType");
-            if($userType == "Client"){
-            header("Location: ../Client.php");
-            exit();
-            
-        }elseif($userType == "admin"){  
+           if($userType == "admin"){  
             header("Location: ../admin.php");
             exit();
-
         
+        }elseif($userType == "Client"){ 
+            header("Location: ../Client.php");
+            exit();
         }else{
+            unset($_SESSION["control"]);
+            header("Location: ../login.php");
+            exit();
+        
+        }
+    
+    }else{
+            
+            header("Location: ../login.php");
+            exit();
+        
+        }
+
+}else{
             
             header("Location: ../login.php");
             exit();
         
         }
     
-    
-
     }
-
-    }
-
-
-}
-    
-    
     
     //logout process
     if(isset($_GET["logout"])){
@@ -79,31 +91,8 @@
             exit();
         
     }
-
-
-// Delete user process
     
-    if(isset($_GET["DeleteUser"])){
-    $DeleteUser = mysqli_real_escape_string($dbConn, $_GET["DeleteUser"]);
-    
-    $DeleteUser_SQL = "DELETE FROM tbl_users WHERE userId = '$DeleteUser' LIMIT 1";
-     
-    $uDelete_res = $dbConn->query($DeleteUser_SQL);
-    if($DeleteUser_SQL === TRUE){
-        header("Location: ../admin/manageAccount.php");
-            exit();
-        
-        }else{
-        print $dbConn->error;
-        header("Location: ../admin/manageAccount.php");
-            exit();
-    }
-}
-
-
-
-
-// Delete user process
+    // Delete user process
     
     if(isset($_GET["DeleteUser"])){
     $DeleteUser = mysqli_real_escape_string($dbConn, $_GET["DeleteUser"]);
@@ -128,12 +117,15 @@
 if (isset($_POST["update"])){
     $firstname = mysqli_real_escape_string($dbConn, $_POST["firstname"]);
     $lastname = mysqli_real_escape_string($dbConn, $_POST["lastname"]);
-    $gender = mysqli_real_escape_string($dbConn, $_POST["gender"]); 
+    $othername = mysqli_real_escape_string($dbConn, $_POST["othername"]);
+    $gender = mysqli_real_escape_string($dbConn, $_POST["gender"]);
+    $dob = mysqli_real_escape_string($dbConn, $_POST["dob"]);
+    $username = mysqli_real_escape_string($dbConn, $_POST["username"]);
     $email = mysqli_real_escape_string($dbConn, $_POST["email"]);
     $userId = mysqli_real_escape_string($dbConn, $_POST["userId"]);
     
 
-    $user_update= "UPDATE tbl_users SET firstname ='$firstname',lastname ='$lastname',dob ='$dob',gender ='$gender',email ='$email' WHERE userId='$userId' LIMIT 1";
+    $user_update= "UPDATE tbl_users SET firstname ='$firstname',lastname ='$lastname',othername ='$othername',gender ='$gender',dob ='$dob',username ='$username',email ='$email' WHERE userId='$userId' LIMIT 1";
     
     
     if($dbConn->query($user_update) === TRUE){
@@ -149,6 +141,5 @@ if (isset($_POST["update"])){
 }
 
  
-?>
 
-
+?> 
