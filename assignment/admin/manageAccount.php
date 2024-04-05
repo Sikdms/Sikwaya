d92b91f381bd15fe1399d307524d90acdce7ab0d
sikwaya lwanzo <129254070+Sikdms@users.noreply.github.com>
2024-04-05 17:16:17 +0300

    manageAccount.php

--- assignment/admin/manageAccount.php
+++ assignment/admin/manageAccount.php
@@ -9,7 +9,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>home page </title>
+    <title>manage account </title>
     <link rel="stylesheet" href="../css/style.css" />
     
   </head>
@@ -30,16 +30,22 @@
     <div class="img"></div>
     <div class="center">
       
-
-
-  <table class="table table-hover">
+    <!--Home-content-->
+  
+    <div class="home-content">
+    <div class="bootstrap-three">
+    <div class="container">
+    
+     <table class="table table-hover">
   <thead>
     <tr>
       <th scope="col">No</th>
       <th scope="col">Id</th>
       <th scope="col">First name</th>
     <th scope="col">Last name</th>
+      <th scope="col">Dob</th>
       <th scope="col">Gender</th>
+    <th scope="col">User name</th>
       <th scope="col">Email</th>
       <th scope="col">User Type</th> 
     <th scope="col">Actions</th>
@@ -56,17 +62,19 @@
       <td><?php print $row_tbl_users["userId"];?></td>
       <td><?php print $row_tbl_users["firstname"];?></td>
       <td><?php print $row_tbl_users["lastname"];?></td>
+      <td><?php print $row_tbl_users["dob"];?></td>
       <td><?php print $row_tbl_users["gender"];?></td>
+      <td><?php print $row_tbl_users["username"];?></td>
       <td><?php print $row_tbl_users["email"];?></td>
       <td><?php print $row_tbl_users["userType"];?></td>
     
      <td>  
-        <button><a href="editUser.php?EditUser=<?php print $row_tbl_users["userId"];?>">Edit</a> </button>
-        <button><a href="../processes/signup_process.php?DeleteUser=<?php print $row_tbl_users["userId"];?>">Delete</a></button>
-        
-        
+    <button><a href="editUser.php?EditUser=<?php print $row_tbl_users["userId"];?>">Edit</a> </button>
+    <button><a href="../processes/signup_process.php?DeleteUser=<?php print $row_tbl_users["userId"];?>">Delete</a></button>
+    
+    
 
-        </td>
+    </td>
     </tr>
       
   <?php } ?>
@@ -74,10 +82,9 @@
    
   </tbody>
 </table>
-
-
-
-      
     </div>
+    </div>
+    </div>
+    
   </body>
 </html>
