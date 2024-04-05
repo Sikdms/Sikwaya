fddd3a792605661714d5c046ba10e89300bcae69
sikwaya lwanzo <129254070+Sikdms@users.noreply.github.com>
2024-04-05 17:15:21 +0300

    EditUser.php

--- assignment/admin/editUser.php
+++ assignment/admin/editUser.php
@@ -1,16 +1,16 @@
- <?php
-	session_start();
-	require_once "../config/dbConnect.php";
-	
-	?>
 <?php
-	if(isset($_GET["EditUser"])){
-	$EditUser = mysqli_real_escape_string($dbConn, $_GET["EditUser"]);
-	
-	$res_tbl_users = $dbConn->query("SELECT * FROM `tbl_users`   WHERE userId = '$EditUser' LIMIT 1"); 
-	$row_tbl_users = $res_tbl_users->fetch_assoc();
-	
-	}
+  session_start();
+  require_once "../config/dbConnect.php";
+  
+  ?>
+<?php
+  if(isset($_GET["EditUser"])){
+  $EditUser = mysqli_real_escape_string($dbConn, $_GET["EditUser"]);
+  
+  $res_tbl_users = $dbConn->query("SELECT * FROM `tbl_users`   WHERE userId = '$EditUser' LIMIT 1"); 
+  $row_tbl_users = $res_tbl_users->fetch_assoc();
+  
+  }
  ?>
 <!DOCTYPE html>
 
@@ -18,7 +18,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>home page </title>
+    <title>edit user </title>
     <link rel="stylesheet" href="../css/style.css" />
     
   </head>
@@ -39,9 +39,11 @@
     <div class="img"></div>
     <div class="center">
       
-
-
-    <form class="needs-validation" action = "../processes/signup_process.php" method = "POST" >
+    <div class="row">
+        
+        <div class="col-md-8 order-md-1">
+          <h4 class="mb-3" style="text-align: center;"></h4>
+          <form class="needs-validation" action = "../processes/signup_process.php" method = "POST" >
           <div class="row">
               <div class="col-md-6 mb-3">
                 <label for="firstname">First name:</label>
@@ -57,24 +59,59 @@
                   
                 </div>
               </div> 
-			 
-			  
+        <div class="col-md-6 mb-3">
+                <label for="othername">Other name:</label>
+                <input type="text" class="form-control" id="othername" name="othername" placeholder="Update othername" value="<?php print $row_tbl_users["othername"];?>" required>
+                <div class="invalid-feedback">
+                
+                </div>
+              </div>
+        
+        <div class="col-md-6 mb-3">
+        <label for="gender">Update Gender:</label>
+        <select class="form-control" id="gender" name = "gender" >
+      <option for="gender" value = "<?php print $row_tbl_users["gender"];?>"><?php print $row_tbl_users["gender"];?></option>
+      <option for="gender" value = "">--Select gender--</option>
+      <option value = "Male">M</option>
+      <option value = "Female">F</option>
+      </select>
+       <div class="invalid-feedback">
+                  
+                </div>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="username">User name:</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Update user name" value="<?php print $row_tbl_users["username"];?>" required>
+                <div class="invalid-feedback">
+                  
+                </div>
+              </div>
+        
+              <div class="col-md-6 mb-3">
+                <label for="dob">Date of birth:</label>
+                <input type="date" class="form-control" id="dob" name="dob" placeholder="" value="<?php print $row_tbl_users["dob"];?>" required>
+                <div class="invalid-feedback">
+                  
+                </div>
+              </div>
             </div>
 
-            <div class="invalid-feedback">
+            <div class="mb-3">
+              <label for="email">Email address:</label>
+              <input type="email" class="form-control" id="email" name="email"placeholder="Update email" value="<?php print $row_tbl_users["email"];?>" required>
+         </div>
+              <div class="invalid-feedback">
             <input type="hidden"  name = "userId" value="<?php print $row_tbl_users["userId"]; ?>" />
               </div>
             
-			
+      
             <hr class="mb-4">
             <button class="btn btn-primary btn-lg btn-block" type="submit" name = "update">Update <?php print $row_tbl_users["firstname"];?></span><?php print "&nbsp";?><span><?php  print $row_tbl_users ["lastname"];?></span></button>
-			
-			
-			
-          </form>
-
-
       
-    </div>
+      
+      
+          </form>
+        </div>
+  
   </body>
 </html>
