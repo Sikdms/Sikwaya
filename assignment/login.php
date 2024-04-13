adc4340975f6597fd0681982d830e40a6880fe25
sikwaya lwanzo <129254070+Sikdms@users.noreply.github.com>
2024-04-13 14:46:35 +0300

    login.php

--- assignment/login.php
+++ assignment/login.php
@@ -12,13 +12,13 @@
     <nav>
       <div class="menu">
         <div class="logo">
-          <a href="index.html">Sikwaya</a>
+          <a href="login.php">Sikwaya</a>
         </div>
         <ul>
-          <li><a href="about.html">About</a></li>
-          <li><a href="Services.html">Services</a></li>
-          <li><a href="contact.html">Contact</a></li>
-          <li><a href="feedback.html">Feedback</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="Services.php">Services</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="feedback.php">Feedback</a></li>
         </ul>
       </div>
     </nav>
