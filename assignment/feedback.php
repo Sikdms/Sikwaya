699dc46080d0707bc5195c3ebb2be4095638fff6
sikwaya lwanzo <129254070+Sikdms@users.noreply.github.com>
2024-04-13 14:44:22 +0300

    feedback.php

--- assignment/feedback.php
+++ assignment/feedback.php
@@ -11,13 +11,13 @@
     <nav>
       <div class="menu">
         <div class="logo">
-          <a href="index.html">Sikwaya</a>
+          <a href="feelback.php">Sikwaya</a>
         </div>
         <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="about.html">About</a></li>
-          <li><a href="Services.html">Services</a></li>
-          <li><a href="contact.html">Contact</a></li>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="Services.php">Services</a></li>
+          <li><a href="contact.php">Contact</a></li>
           
         </ul>
       </div>
